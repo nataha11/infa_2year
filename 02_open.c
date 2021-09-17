dc0d6dc632495b3b2160614ba2c86fa0f1117fea
Natallia Kuzmina <kuzmina10n@gmail.com>
2021-09-18 00:08:54 +0300

    open from sem

---
+++ 02_open.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+
+ssize_t writeall(int fd, const void *buf, size_t count) {
+	size_t bytes_written = 0;
+	const uint8_t *buf_addr = buf;
+	while (bytes_written < count) {
+		ssize_t res = write(fd, buf_addr + bytes_written, count - bytes_written);
+		if (res < 0) {
+			return res;
+		}
+		bytes_written += res;
+	
+	}
+	return (ssize_t)bytes_written;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 3){
+		fprintf(stderr, "Usage: %s path text\n", argv[0]);
+		return 1;
+	}
+
+	int fd = open (argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fd < 0) {
+        perror("Failed to open file for writing");
+		return 2;
+    }
+
+	if (writeall(fd, argv[2], strlen(argv[2])) < 0) {
+		perror ("Failed to write");
+		close(fd);
+		return 3;
+	}
+
+	if (close(fd) < 0) {
+		perror("Failure during close");
+		return 4;
+	}
+
+	return 0;
+}
