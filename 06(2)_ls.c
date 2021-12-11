b29166f80d0ab458583f0d5f66aba215509fa179
Natallia Kuzmina <kuzmina10n@gmail.com>
2021-12-12 00:47:52 +0300

    remove odd includes

--- 06(2)_ls.c
+++ 06(2)_ls.c
@@ -1,13 +1,8 @@
-#define _DEFAULT_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <errno.h>
 
 char stattype(unsigned mode) {
     switch (mode & S_IFMT) {
