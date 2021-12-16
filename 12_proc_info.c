d9f77059eea88ae5e8f4cf25d00eba4a49f9be4a
Natallia Kuzmina <kuzmina10n@gmail.com>
2021-12-16 22:48:14 +0300

    разыменование NULL fixed

--- 12_proc_info.c
+++ 12_proc_info.c
@@ -4,32 +4,56 @@
 #include <limits.h>
 #include <grp.h>
 #include <pwd.h>
+#include <string.h>
+#include <stdlib.h>
 
 void proc_info(void) {
 
     printf("PID %d, PPID %d\n", getpid(), getppid());
     printf("\tPGID %d, SID %d\n", getpgrp(), getsid(0));
-    printf("\tUID %d(%s), GID %d(%s)\n", getuid(), getpwuid(getuid())->pw_name, getgid(), getgrgid(getgid())->gr_name);
-    printf("\tEUID %d(%s), EGID %d(%s)\n", geteuid(), getpwuid(geteuid())->pw_name, getegid(), getgrgid(getegid())->gr_name);
+    
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+    //чтобы не было разыменования нулевого указателя
+    char * unknown = malloc(sizeof("?"));
+    strcpy(unknown, "?");
+    struct passwd * pwuid = getpwuid(uid);
+    char * pwname = pwuid ? pwuid->pw_name : unknown;
+    struct group * grid = getgrgid(gid);
+    char * grname = grid ? grid->gr_name : unknown;
+
+    printf("\tUID %d(%s), GID %d(%s)\n", uid, pwname, gid, grname);
+
+    uid_t euid = geteuid();
+    gid_t egid = getegid();
+    //чтобы не было разыменования нулевого указателя
+    struct passwd * epwuid = getpwuid(euid);
+    char * epwname = epwuid ? epwuid->pw_name : unknown;
+    struct group * egrid = getgrgid(egid);
+    char * egrname = egrid ? egrid->gr_name : unknown;
+
+    printf("\tEUID %d(%s), EGID %d(%s)\n", euid, epwname, egid, egrname);
 
     //get supplementary groups
-    long ngroups_max;//65536
-    if((ngroups_max = sysconf(_SC_NGROUPS_MAX)) == -1) {
-        perror("sysconf");
-        return;
-    }
-    gid_t list[ngroups_max];
+    // If size is 0, the total number of supplementary group IDs for the process is returned.
+    int ngroups = getgroups(0, NULL);
+
+    gid_t list[ngroups];
     int num_groups;
-    if ((num_groups = getgroups((int)ngroups_max + 1, list)) == -1) {
+    if ((num_groups = getgroups(ngroups + 1, list)) == -1) {
         perror("getgroups");
         return;
     }
 
     printf("Supplementary groups(%d): ", num_groups);
     for (int i = 0; i < num_groups; i++) {
-        printf("%d(%s) ", list[i], getgrgid(list[i])->gr_name);
+        //чтобы не было разыменования нулевого указателя
+        struct group * ngrid = getgrgid(list[i]);
+        char * grname = ngrid ? ngrid->gr_name : unknown;
+        printf("%d(%s) ", list[i], grname);
     }
     printf("\n");
+    free(unknown);
 }
 
 int main(void) {
