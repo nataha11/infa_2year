dd0e66fc5e31073a1d40f532701af3393540e3bf
Natallia Kuzmina <kuzmina10n@gmail.com>
2021-12-20 18:06:17 +0300

    while(1)

--- 13(1)_waitpid.c
+++ 13(1)_waitpid.c
@@ -16,35 +16,41 @@ int main(void) {
 
     if (child_id == 0) {
         //this code is executed in child process only
-        proc_info("child try 1");
+        proc_info("child try");
         pause();//waiting for signal(s)
-        return 0;//any value as exit code
+        return 0;
     }
 
     //this code is executed in parent process only
     proc_info("parent");
     int status;
-    do {
+    
+    while(1) {
         pid_t res = waitpid(child_id, &status, WUNTRACED | WCONTINUED); 
         //waitpid returns the process ID of the terminated\resumed\stopped child
-        if(res == -1) {
+        if (res == -1) {
             perror("waitpid");
             return 2;
         }
-//todo: rewrite the following code
-        if(WIFEXITED(status)) {
-            printf("child with PID = %d has exited normally with code %d\n", res, WEXITSTATUS(status));
-        } else if (WIFSIGNALED(status)) {
-            if(WCOREDUMP(status))
-                printf("child with PID = %d  produced a core dump\n", res);
-            else 
-                printf("child with PID = %d has been killed by signal %d\n", res, WTERMSIG(status));
-        } else if (WIFSTOPPED(status))
-            printf("child with PID = %d has been stopped by signal %d\n", res, WSTOPSIG(status));
-        else if (WIFCONTINUED(status))
-            printf("child with PID = %d continued\n", res);
 
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        if (WIFEXITED(status)) {
+            printf("child with PID = %d has exited normally with code %d\n", res, WEXITSTATUS(status));
+            break;
+        } else if (WIFSIGNALED(status)) {//child terminated
+            if (WCOREDUMP(status))
+                printf("child with PID = %d  produced a core dump\n", res);            
+            printf("child with PID = %d has been killed by signal %d\n", res, WTERMSIG(status));
+            break;
+        } else if (WIFSTOPPED(status)) {
+            printf("child with PID = %d has been stopped by signal %d\n", res, WSTOPSIG(status));
+        } else if (WIFCONTINUED(status)) {
+            printf("child with PID = %d continued\n", res);
+        } else {
+            printf("Something weird has happened\n");
+            break;
+        }
+
+    }
 
     return 0;
 }
